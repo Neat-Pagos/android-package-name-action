9fb9eed99feb16d37bd0164b318c577481145d35
Javier Benavides <javier@neatpagos.com>
2024-03-27 18:45:37 -0300

    Add console logs and improve package name override logic

--- index.js
+++ index.js
@@ -14,6 +14,13 @@ try {
     const mainApplicationPath = core.getInput('mainApplicationPath');
     const newPackageName = core.getInput('newPackageName');
     const oldPackageName = core.getInput('oldPackageName');
+    console.log(`newPackageName: ${newPackageName}`);
+    console.log(`oldPackageName: ${oldPackageName}`);
+    console.log(`androidManifestPath: ${androidManifestPath}`);
+    console.log(`buildGradlePath: ${buildGradlePath}`);
+    console.log(`stringsPath: ${stringsPath}`);
+    console.log(`mainActivityPath: ${mainActivityPath}`);
+    console.log(`mainApplicationPath: ${mainApplicationPath}`);
     const searchRegExp = new RegExp(oldPackageName, 'g');
     const paths = [
         androidManifestPath,
@@ -24,15 +31,18 @@ try {
     ];
 
     Promise.allSettled(paths.map(async (path) => {
+        console.log(`Processing ${path}`);
         const pathData = await readFile(path, 'utf8');
         let newData = pathData.replace(searchRegExp, newPackageName);
         await writeFile(gradlePath, newData);
         console.log(`Successfully override package name ${newPackageName} on ${path}`);
+        return;
     })).then(() => {
+        console.log(`Successfully override package name ${newPackageName}`);
         core.setOutput("result", `Done`);
     });
+    console.log(`SYNC - Successfully override package name ${newPackageName}`);
     
-
 } catch (error) {
     core.setFailed(error.message);
 }
